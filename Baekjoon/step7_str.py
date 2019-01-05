4dcad616d6df35811d2fbf9ba1c9539ee5a82e00
unknown <theone0920@korea.ac.kr>
2019-01-05 18:52:30 +0900

    D01 | 190105_1st | Baekjoon

--- Baekjoon/step7_str.py
+++ Baekjoon/step7_str.py
@@ -2,10 +2,11 @@
 
 # # 10809. 알파벳찾기
 
+# from string import ascii_lowercase
 # S = input()
 # value = 0
 # B =[]
-# A = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# A = list(ascii_lowercase)
 
 # for i in range(len(A)):
 #     for j in range(len(S)):
@@ -17,19 +18,100 @@
 #     B.append(value)
 # print(*B)
 
-# 2675. 문자열 반복
+# # 2675. 문자열 반복
 
-import sys
+# T = int(input())
+# C = T
+# A = [0, 0]
 
-T = int(input())
-C = T
-A = []
+# while C:
+#     A[0], A[1] = input().split()
+#     C -= 1
+#     for j in range(len(A[1])):
+#         print(A[1][j]*int(A[0]), end ='')
+#     print()
+#     if C == 0 : break
 
-while C:
-    A.append(sys.stdin.readline().split())
-    C -= 1
-    if C == 0 : break
-for i in range(T):
-    for j in range(len(A[i][1])):
-        A[i][1] = A[i][1][j] *A[i][0]
-print(A)
+# 1157. 단어 공부
+# from string import ascii_uppercase
+
+# B = [0 for i in range(26)]
+# Alpha = input().upper()
+# return_Alpha = 0
+# a = 0
+
+# for i in range(26):
+#     for j in range(len(Alpha)):
+#         if Alpha[j] == ascii_uppercase[i]:
+#             B[i] += 1
+
+# if B.count(max(B)) == 1:
+#     for i in range(len(B)):
+#         if max(B) == B[i]:
+#             print(ascii_uppercase[i])
+# else:
+#     print("?")
+
+# # 1316. 그룹단어체커
+
+# N = int(input())
+# A =[]
+# Prove = []
+# snumber = 0
+
+# for i in range(N):
+#     a = input()
+#     A.append(a)
+#     for k in range(len(A[i])):
+#         if A[i][k] not in Prove:
+#             Prove.append(A[i][k])
+#         elif A[i][k] != A[i][k-1]:
+#             break
+#     else:
+#         snumber += 1
+#     Prove = []
+# print(snumber)
+
+# # 2908. 상수
+
+# def sangsu(sentenceA, sentenceB):
+#     setenceA = int(sentenceA)
+#     setenceB = int(sentenceB)
+#     if sentenceA >= sentenceB:
+#         return sentenceA
+#     else:
+#         return sentenceB
+
+# numberA , numberB = input().split()
+# numberA = numberA[::-1]
+# numberB = numberB[::-1]
+# print(sangsu(numberA, numberB))
+
+# # 5622. 다이얼
+
+# dial = {3:['A','B','C'], 4:['D','E','F'], 5:['G','H','I'], 6:['J','K','L'], 7:['M','N','O'], 8:['P','Q','R','S'], 9:['T','U','V'], 10:['W','X','Y','Z']}
+# num = input()
+# return_number = 0
+
+# for i in range(len(num)):
+#     for j in range(3,11):
+#         if num[i] in dial[j]:
+#             return_number += j
+
+# print(return_number)
+
+# 2941. 크로아티아 알파벳
+iValue = input()
+rValue = 0
+for i in range(len(iValue)):
+    if iValue[i] == '=':
+        rValue -= 1
+        if iValue[i-1] == 'z' and iValue[i-2] == 'd':
+            rValue -= 1
+    elif iValue[i] == '-':
+        rValue -= 1
+    elif iValue[i] == 'j':
+        if iValue[i-1] == 'l' or iValue[i-1] == 'n':
+            rValue -= 1
+    rValue +=1
+print(rValue)
